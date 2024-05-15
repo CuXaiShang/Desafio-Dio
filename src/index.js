75299b02c770c10867281855438aa09e086326da
Victor <victoralves1942@hotmail.com>
2024-05-15 18:33:43 -0300

    Adicionado arquivo HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+let n1 = 1
+let n2 = 2
+
+let s = n1 + n2
+
+console.log (s)
+
 console.log("Desafio DIO")
 
 console.log("Uma outra coisa escrita onde nao tem nada haver com a primeira so para fazer um teste")
